bf9f04fe8bb87ee2b987858cd7fb6a1a71962b5d
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2023-12-09 20:31:47 -0300

    added class Questao5

---
+++ src/Questao5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Questao5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insira o tempo gasto na viagem em horas:");
+        double travelTime = input.nextDouble();
+
+        System.out.println("Insira a velocidade média durante a viagem em Km/h:");
+        double speed = input.nextDouble();
+
+        System.out.printf("São necessários %.3f litros de combustível para a sua viagem.", fuelConsumption(travelTime, speed));
+    }
+
+    public static double fuelConsumption(double travelTime, double speed){
+        return (travelTime * speed) / 12;
+    }
+}
+
